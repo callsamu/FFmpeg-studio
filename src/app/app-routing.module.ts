ce8bfdd232e8caa4b21ecd31f380c956990ad37d
callsamu <samuelbree@gmail.com>
2023-04-18 15:46:50 -0300

    feat: add route to ffmpeg process component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditorComponent } from './editor/editor.component';
+import { FFmpegProcessComponent } from './ffmpeg-process/ffmpeg-process.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'new', pathMatch: 'full'},
   { path: 'new', component: EditorComponent },
+  { path: 'run', component: FFmpegProcessComponent },
 ];
 
 @NgModule({
