9ec937dfdf76005a01f411694d18bad481681224
callsamu <samuelbree@gmail.com>
2023-04-18 00:44:04 -0300

    feat: use routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditorComponent } from './editor/editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'new', pathMatch: 'full'},
+  { path: 'new', component: EditorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
