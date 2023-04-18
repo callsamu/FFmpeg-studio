85714097cd3d2716254f0323803b21e4de2649d9
callsamu <samuelbree@gmail.com>
2023-04-18 16:19:15 -0300

    feat: implement basic version of ffmpeg process component

--- src/app/ffmpeg-process/ffmpeg-process.component.ts
+++ src/app/ffmpeg-process/ffmpeg-process.component.ts
@@ -8,13 +8,27 @@ import { FFmpegService } from '../ffmpeg.service';
   styleUrls: ['./ffmpeg-process.component.scss']
 })
 export class FFmpegProcessComponent {
+  ready = true;
+  progress = 0;
+  log = "";
+
+
   constructor(
     private argsService: ArgsService,
     private ffmpegService: FFmpegService,
   ) {}
 
   ngOnInit(): void {
+    this.ffmpegService.progress()
+      .subscribe(x => this.progress = x);
+    this.ffmpegService.logs()
+      .subscribe(log => this.log += log.message + '\n');
 
+    if (!this.ffmpegService.ready) {
+      this.ready = false;
+      return;
+    }
 
+    this.ffmpegService.run(this.argsService.getArgs());
   }
 }
