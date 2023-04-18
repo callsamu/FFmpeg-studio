0d9bf239c70cf40c88a065a3f842eaf6944f32fb
callsamu <samuelbree@gmail.com>
2023-04-18 00:39:18 -0300

    feat: implement basic version of EditorComponent

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./editor.component.scss']
 })
 export class EditorComponent {
+  command = "ffmpeg";
+  files: File[] = [];
 
+  run(): void {
+    console.log("running ffmpeg:");
+    console.log(this.files.map(file => file.name));
+
+    const args = this.command
+      .split(/\s|\n/)
+      .filter(arg => arg !== "");
+
+    console.log(args);
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.currentTarget as HTMLInputElement;
+    let fileList = input.files;
+
+    if (fileList) {
+      for (let i = 0; i < fileList.length; i ++) {
+        const file = fileList[i];
+        this.command += " " + file.name;
+        this.files.push(fileList[i]);
+      }
+    }
+  }
 }
