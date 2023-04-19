75031b1a4c67157b523b389b1d891dd38072c7e7
callsamu <samuelbree@gmail.com>
2023-04-19 20:36:31 -0300

    chore: remove unused variables, types

--- src/app/code-editor/code-editor.component.ts
+++ src/app/code-editor/code-editor.component.ts
@@ -1,12 +1,4 @@
-import { OnChanges, AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild, SimpleChanges } from '@angular/core';
-import hljs from 'highlight.js';
-import { CodeJar, Position } from 'codejar';
-import { FFmpegSyntax } from '../syntax';
-
-type UpdateEvent = {
-  code: string;
-  cursor: Position;
-}
+import { OnChanges, AfterViewInit, Component, ElementRef, Input, ViewChild, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-code-editor',
@@ -15,54 +7,10 @@ type UpdateEvent = {
 })
 export class CodeEditorComponent implements OnChanges, AfterViewInit {
   @ViewChild('editor') element?: ElementRef;
-  editor?: CodeJar;
 
   @Input() code = "";
-  @Output() codeChange = new EventEmitter<UpdateEvent>;
 
-  ngAfterViewInit(): void {
-    if (!this.element) return;
+  ngAfterViewInit(): void {}
 
-    hljs.registerLanguage('ffmpeg', FFmpegSyntax);
-
-    const highlight = (e: HTMLElement) => {
-      const code = e.textContent ?? "";
-      const highlight = hljs.highlight(code, { language: 'ffmpeg' });
-      e.innerHTML = highlight.value;
-    }
-
-    const OPTIONS = {
-      tab: "  ",
-      spellCheck: true,
-    }
-
-    this.editor = CodeJar(
-      this.element.nativeElement,
-      highlight,
-      OPTIONS,
-    );
-
-    this.editor.onUpdate(code => {
-      if (!this.editor) return;
-      this.codeChange.emit({
-        code: code,
-        cursor: this.editor.save()
-      });
-    });
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (!this.editor) return;
-
-    const newCode = changes["code"].currentValue as string;
-    const charDifference = newCode.length - this.editor.toString().length;
-
-    const pos = this.editor.save();
-    this.editor.updateCode(newCode);
-
-    pos.start += charDifference;
-    pos.end += charDifference;
-
-    this.editor.restore(pos);
-  }
+  ngOnChanges(changes: SimpleChanges): void {}
 }
