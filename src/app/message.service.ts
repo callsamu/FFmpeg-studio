6496365b05f04a17a22007c144ced93c67162ba9
callsamu <samuelbree@gmail.com>
2023-04-21 12:56:01 -0300

    feat: use behaviour subject for message service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { Message } from './message';
+import { BehaviorSubject, debounceTime, Observable, tap } from 'rxjs';
+import { Message, MessageType } from './message';
+
+const initMessage = {
+  content: "Welcome to FFmpeg Studio!",
+  type: MessageType.Info,
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
-  messages = new Subject<Message>;
+  messages = new BehaviorSubject<Message>(initMessage);
 
-  constructor() { }
+  constructor() {}
 
   observable(): Observable<Message> {
-    return this.messages.asObservable();
+    return this.messages.asObservable().pipe(
+      tap(x => console.log(x)),
+      debounceTime(100)
+    );
   }
 
   setMessage(message: Message) {
--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { Message, MessageType } from '../message';
 import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+  styleUrls: ['./message.component.scss'],
+  animations: [
+    trigger('write', [
+      state('hidden', style({
+        width: "0%",
+      })),
+      state('written', style({
+        width: "100%",
+      })),
+      transition('hidden => written', [
+        animate('5s')
+      ]),
+    ])
+  ]
 })
-export class MessageComponent {
+
+export class MessageComponent implements OnInit {
+  message?: Message;
+  changed = false;
+  types = MessageType;
+
   constructor(
-    public messageService: MessageService,
+    private messageService: MessageService,
   ) {}
+
+  ngOnInit(): void {
+    this.messageService.observable().subscribe(message => {
+      this.changed = false;
+      this.message = message;
+      this.changed = true;
+    })
+  }
 }
