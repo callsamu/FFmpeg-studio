e9865445281afccd49048f1cd51bf0bb911f0807
callsamu <samuelbree@gmail.com>
2023-04-22 20:37:13 -0300

    feat: add sidebar sections for command process

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FFmpegService } from './ffmpeg.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ffmpeg-studio';
 
-  constructor() {}
+  constructor(
+    public ffmpegService: FFmpegService,
+  ) {}
 }
