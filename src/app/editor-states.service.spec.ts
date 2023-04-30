f8dc06f8fd548d5a39816ff1ffe260ca2303ea32
callsamu <samuelbree@gmail.com>
2023-04-29 21:39:58 -0300

    feat: test editor state service

--- src/app/editor-states.service.spec.ts
+++ src/app/editor-states.service.spec.ts
@@ -1,16 +1,47 @@
 import { TestBed } from '@angular/core/testing';
+import { StorageService } from 'src/storage.service';
+import { ArgsService } from './args.service';
 
 import { EditorStatesService } from './editor-states.service';
+
+const command = "ffmpeg -i foobar.mkv foo.mp4";
 
 describe('EditorStatesService', () => {
   let service: EditorStatesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const spyStorage = jasmine.createSpyObj('StorageService', {
+      fetch: command,
+    });
+
+    const spyArgs = jasmine.createSpyObj('ArgsService', {
+      files: new Map<string, File>(),
+    });
+
+    TestBed.configureTestingModule({ providers: [
+      { provide: ArgsService, useValue: spyArgs },
+      { provide: StorageService, useValue: spyStorage }
+    ]});
+
     service = TestBed.inject(EditorStatesService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('#fetch created editor state', () => {
+    it(`should be a new command if command name name is null`, () => {
+
+      const state = service.fetch(null);
+      expect(state.doc.toString()).toBe('ffmpeg');
+    })
+    it(`state for command name should contain it's document in storage`, () => {
+      shouldReturnSomething = true;
+
+      const state = service.fetch("command");
+      console.log()
+      expect(state.doc.toString()).toBe(command);
+    });
   });
 });
