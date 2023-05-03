55f3f97c45a598135d84351d3a2b061139936533
callsamu <samuelbree@gmail.com>
2023-05-03 00:36:05 -0300

    feat: test message component

--- src/app/message/message.component.spec.ts
+++ src/app/message/message.component.spec.ts
@@ -1,14 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BehaviorSubject } from 'rxjs';
+import { MessageType } from '../message';
+import { MessageService } from '../message.service';
 
 import { MessageComponent } from './message.component';
+
+const message = {
+  type: MessageType.Info,
+  content: 'foo',
+};
 
 describe('MessageComponent', () => {
   let component: MessageComponent;
   let fixture: ComponentFixture<MessageComponent>;
 
   beforeEach(async () => {
+
+    const messageSpy = jasmine.createSpyObj('MessageService', {
+      observable: new BehaviorSubject(message),
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ MessageComponent ]
+      declarations: [ MessageComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        MatProgressSpinnerModule,
+        MatIconModule,
+      ],
+      providers: [
+        { provide: MessageService, useValue: messageSpy },
+      ],
     })
     .compileComponents();
 
@@ -19,5 +43,9 @@ describe('MessageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should receive messages', () => {
+    expect(component.message).toEqual(message);
   });
 });
